a0c84ee49d81d72057ce10340f31bf2dd1a70efa
Amaresha07 <151951300+Amaresha07@users.noreply.github.com>
2024-10-20 22:20:42 +0530

    no issues

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {goto} from "$app/navigation";
+    import { onMount } from "svelte";
+
+    let login = () => {
+        goto("/login");
+    }
+
+    onMount(() => {
+        login();
+    });
+</script>
+
+<button class="border-2 border-black pl-4 pr-4 pt-2 pb-2 hover:bg-black hover:text-white duration-300">Click</button>
+
+<h1 class="text-4xl font-bold hover:text-green-400 cursor-pointer">Hello</h1>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {goto} from "$app/navigation";
+    let login = () => {
+        goto("/home");
+    }
+</script>
+
+
+<div class="w-full h-screen border border-black flex justify-center items-center">
+    <div class="w-96 h-96 border-2 border-black rounded-lg flex flex-col justify-center items-center p-4">
+        <i class="fa-solid fa-right-to-bracket text-4xl mb-5"></i>
+        <h1 class="text-2xl font-semibold pb-6">Login</h1>
+        <input type="email" name="Email" placeholder="Email" class="border border-black rounded-md w-full h-12 pl-2" required>
+        <input type="password" name="Email" placeholder="Password" class="border border-black rounded-md w-full h-12 mt-4 pl-2" required>
+        <button class="w-full h-12 bg-black text-white text-xl mt-4 rounded-xl text-center font-semibold hover:bg-white duration-500 hover:text-black hover:border-2 hover:border-black" on:click={login}>Login</button>
+    </div>
+</div>
